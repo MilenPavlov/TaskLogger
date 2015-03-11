60c6e07cc2b90cf56c44d2510dfe3285034365e4
MilenPavlov <milenppavlov@gmail.com>
2015-03-11 07:29:51 +0000

    added response classes

---
+++ TaskLogger.Data/Responses/UserTasksResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskLogger.Data.Responses
+{
+    using TaskLogger.Data.Models;
+
+    public class UserTasksResponse : ResponseBase
+    {
+        public IEnumerable<UserTask> UserTasks { get; set; }
+    }
+}
