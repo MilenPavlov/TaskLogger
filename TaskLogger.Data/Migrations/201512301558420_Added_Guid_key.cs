ffb39cce8df89530e4bc04289b2eeb70f6292d64
milenpavlov <milenpavlov@etech.net>
2015-12-30 16:03:56 +0000

    Added unique key

---
+++ TaskLogger.Data/Migrations/201512301558420_Added_Guid_key.cs
@@ -0,0 +1,22 @@
+namespace TaskLogger.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Added_Guid_key : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.UserImages");
+            AlterColumn("dbo.UserImages", "UserImageId", c => c.Guid(nullable: false, identity: true));
+            AddPrimaryKey("dbo.UserImages", "UserImageId");
+        }
+        
+        public override void Down()
+        {
+            DropPrimaryKey("dbo.UserImages");
+            AlterColumn("dbo.UserImages", "UserImageId", c => c.Guid(nullable: false));
+            AddPrimaryKey("dbo.UserImages", "UserImageId");
+        }
+    }
+}
--- TaskLogger.Data/Models/UserImage.cs
+++ TaskLogger.Data/Models/UserImage.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TaskLogger.Data.Models
 {
     public class UserImage
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid UserImageId { get; set; }
         public byte[] ImageBytes { get; set; }
         public virtual  User  User{ get; set; }
